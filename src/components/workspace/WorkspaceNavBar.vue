de4abfc417b08f50eca59270655650d42ca6f557
RostamMahabadi <RostamMahabadi@gmail.com>
2025-01-15 06:11:20 -0600

    remove unused sidebar components

--- src/components/workspace/WorkspaceNavBar.vue
+++ src/components/workspace/WorkspaceNavBar.vue
@@ -61,26 +61,6 @@
         </div>
         <span class="nav-label">DMs</span>
       </div>
-
-      <!-- Activity -->
-      <div class="nav-item">
-        <div class="icon-container">
-          <svg viewBox="0 0 24 24" class="icon">
-            <path fill="currentColor" d="M12 22c1.1 0 2-.9 2-2h-4c0 1.1.89 2 2 2zm6-6v-5c0-3.07-1.64-5.64-4.5-6.32V4c0-.83-.67-1.5-1.5-1.5s-1.5.67-1.5 1.5v.68C7.63 5.36 6 7.92 6 11v5l-2 2v1h16v-1l-2-2z"/>
-          </svg>
-        </div>
-        <span class="nav-label">Activity</span>
-      </div>
-
-      <!-- More -->
-      <div class="nav-item">
-        <div class="icon-container">
-          <svg viewBox="0 0 24 24" class="icon">
-            <path fill="currentColor" d="M12 8c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2zm0 2c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm0 6c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2z"/>
-          </svg>
-        </div>
-        <span class="nav-label">More</span>
-      </div>
     </div>
 
     <div class="nav-bottom">
